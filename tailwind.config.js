8f1ea728a66ea8215494623c982d576ba4b32628
Kegen Guyll <guyllkegen@gmail.com>
2022-05-22 21:13:34 -0500

    added colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ module.exports = {
   ],
   plugins: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#242424',
+        tmrev: {
+          alt: {
+            blue: '#1CBA13',
+            red: '#FD4C55',
+            yellow: '#FFC000',
+          },
+          gray: {
+            dark: '#3B3B3B',
+            light: '#7B7B7B',
+            mid: '#555555',
+
+          },
+          purple: {
+            main: '#430062',
+            secondary: '#340049',
+          },
+        },
+      },
+    },
   },
 };
