aaf5c39ab0742cc98bea4755cd54102317c24c5d
Kegen Guyll <guyllkegen@gmail.com>
2022-07-16 12:14:51 -0500

    added NextNProgress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import React from 'react';
 
 import { wrapper } from '../redux/store';
@@ -9,9 +10,13 @@ import Layout from './layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <NextNProgress color="#FFC000" />
+    </>
+
   );
 }
 
