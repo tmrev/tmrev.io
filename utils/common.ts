286a8ca032d5e08e8fa4c50ead8a05652d1ce3d0
Kegen Guyll <guyllkegen@gmail.com>
2022-07-16 15:54:16 -0500

    created debounce function

--- utils/common.ts
+++ utils/common.ts
@@ -19,3 +19,12 @@ export function extractNameFromEmail(email: string | null):string {
 
   return email.split('@')[0];
 }
+
+export const debounce = (fn: Function, ms = 300) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  // eslint-disable-next-line func-names
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
