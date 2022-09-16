8c17178f5438a8c8ff3cd19986d84843393bb6d5
Kegen Guyll <guyllkegen@gmail.com>
2022-09-15 19:30:42 -0500

    add icon link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <link crossOrigin="" href="https://fonts.gstatic.com" rel="preconnect" />
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
