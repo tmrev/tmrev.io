a1f7699894623be9cbe762be4fcaff5d6572471f
Kegen Guyll <guyllkegen@gmail.com>
2022-09-19 19:28:12 -0500

    fixed stylesheets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Document, {
   DocumentContext, Head, Html, Main, NextScript,
 } from 'next/document';
@@ -12,15 +13,19 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html className="dark">
+      <Html className="dark" lang="en">
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap"
+          <link href="https://fonts.gstatic.com" rel="preconnect" />
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap');
+          </style>
+          {/* <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
             rel="stylesheet"
-          />
-          <link crossOrigin="" href="https://fonts.gstatic.com" rel="preconnect" />
+          /> */}
           <link
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp&display=swap"
             rel="stylesheet"
           />
         </Head>
