ae14ce46bd0167b6feecdf34092b5ae11b0fcff8
Kegen Guyll <guyllkegen@gmail.com>
2022-10-25 17:58:24 -0500

    added new logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp&display=swap"
             rel="stylesheet"
           />
+          <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+          <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+          <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+          <link href="/site.webmanifest" rel="manifest" />
+          <link color="#242424" href="/safari-pinned-tab.svg" rel="mask-icon" />
+          <meta content="#242424" name="msapplication-TileColor" />
+          <meta content="#242424" name="theme-color" />
         </Head>
         <body>
           <Main />
