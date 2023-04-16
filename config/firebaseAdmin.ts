f4b4ae70413cdbf3fa145c7cf64653d2dfbbc0f0
caycekoehler <107483923+caycekoehler@users.noreply.github.com>
2023-04-15 23:06:51 -0400

    Had to make some changes and comment out the full firebaseAdmin.ts. I'm not sure why currently.

--- config/firebaseAdmin.ts
+++ config/firebaseAdmin.ts
@@ -1,13 +1,13 @@
-import * as firebaseAdmin from 'firebase-admin';
+// import * as firebaseAdmin from 'firebase-admin';
 
-if (!firebaseAdmin.apps.length) {
-  firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert({
-      clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-      privateKey: (process.env.NEXT_PUBLIC_PRIVATE_KEY as string).replace(/\n/gm, '\n'),
-      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    }),
-  });
-}
+// if (!firebaseAdmin.apps.length) {
+//   firebaseAdmin.initializeApp({
+//     credential: firebaseAdmin.credential.cert({
+//       clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+//       privateKey: (process.env.NEXT_PUBLIC_PRIVATE_KEY as string).replace(/\n/gm, '\n'),
+//       projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//     }),
+//   });
+// }
 
-export { firebaseAdmin };
+// export { firebaseAdmin };
--- config/firebaseInit.ts
+++ config/firebaseInit.ts
@@ -1,17 +1,20 @@
-import { initializeApp } from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const config = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-};
+import firebaseConfig from './firebaseConfig';
 
-const app = initializeApp(config);
+// const config = {
+//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
+//   appId: process.env.NEXT_PUBLIC_APP_ID,
+//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+// };
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
 // const analytics = getAnalytics(app);
 // logEvent(analytics, 'notification_received');
 const db = getFirestore(app);
