0e0b73856e1bd7a5e7b92ffb8e6aaf5f6f5d1b26
Nabeel77 <nabeel.beeni30@gmail.com>
2023-04-19 11:21:57 +0300

    refactor: changed double quotes on imports to single quotes

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,24 +1,24 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { FunctionComponent, useState } from "react";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { FunctionComponent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
-import { useAppDispatch } from "../../hooks";
-import { useAuth } from "../../provider/authUserContext";
-import { tmrevAPI } from "../../redux/api";
+import { useAppDispatch } from '../../hooks';
+import { useAuth } from '../../provider/authUserContext';
+import { tmrevAPI } from '../../redux/api';
 import {
   Content,
   setClearModal,
   setModalContent,
   setOpenModal,
-} from "../../redux/slice/modalSlice";
-import { handleError } from "../../utils/firebase";
-import Button from "../common/Button";
-import Input from "../common/Input";
-import HeaderText from "../common/typography/headerText";
-import RegisterPanel, { createTMREVAccount } from "../register";
+} from '../../redux/slice/modalSlice';
+import { handleError } from '../../utils/firebase';
+import Button from '../common/Button';
+import Input from '../common/Input';
+import HeaderText from '../common/typography/headerText';
+import RegisterPanel, { createTMREVAccount } from '../register';
 
 interface Props {
   isModal: boolean;
@@ -28,14 +28,14 @@ interface Props {
 const schema = yup.object().shape({
   email: yup
     .string()
-    .email("Must have a valid Email")
-    .required("Email is required"),
-  password: yup.string().required("Password is required"),
+    .email('Must have a valid Email')
+    .required('Email is required'),
+  password: yup.string().required('Password is required'),
 });
 
 const defaultValues = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 const LoginPanel: FunctionComponent<Props> = ({
@@ -53,7 +53,7 @@ const LoginPanel: FunctionComponent<Props> = ({
   const dispatch = useAppDispatch();
   const { signInWithEmailAndPassword, signInWithGoogle } = useAuth();
   const router = useRouter();
-  const [firebaseError, setFirebaseError] = useState<string>("");
+  const [firebaseError, setFirebaseError] = useState<string>('');
 
   const findUserByUid = async (uid: string) => {
     const res = await fetch(`${tmrevAPI}/user/isUser/${uid}`);
@@ -77,9 +77,9 @@ const LoginPanel: FunctionComponent<Props> = ({
         <RegisterPanel isModal redirectPath={`${router.asPath}/review`} />
       ),
       closeFunc: () => dispatch(setOpenModal(false)),
-      description: "enorder to review a movie on trmev you need to login",
+      description: 'enorder to review a movie on trmev you need to login',
       outsideClick: true,
-      title: "Please Login to review",
+      title: 'Please Login to review',
     };
 
     dispatch(setOpenModal(true));
@@ -119,14 +119,14 @@ const LoginPanel: FunctionComponent<Props> = ({
         <Button onClick={onGoogle}>Login With Google</Button>
         <Input
           className="px-3 py-1 border rounded"
-          {...register("email")}
+          {...register('email')}
           error={errors.email}
           placeholder="Email"
           type="email"
         />
         <Input
           error={errors.password}
-          {...register("password")}
+          {...register('password')}
           placeholder="Password"
           type="password"
         />
